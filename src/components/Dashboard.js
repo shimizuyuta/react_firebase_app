6aa0c0ad7e1e9c6de87711dbeedb8b209d088443
clean_water <shmizuyuta213@icloud.com>
2021-10-10 11:59:27 +0900

    firebaseにデータ追加

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,20 +2,26 @@ import React,{useState,useEffect,useContext} from "react";
 import  { AuthContext } from '../providers/AuthProvider';
 import dig from 'object-dig'
 import { signInWithGoogle } from "../service/firebase";
+import * as Api from '../service/api'
 
 const DashBoard = () => {
     const currentUser = useContext(AuthContext);
+    const [inputName,setInputName] = useState(null)
+    console.log(inputName)
     const formRender = () =>{
         let dom
     if(dig(currentUser,'currentUser','uid')){
         dom = <form>
-          <input placeholder='todoName'/>
-          <button>add</button>
+          <input placeholder='ToDoName' onChange={(event)=>setInputName(event.currentTarget.value)}/>
+          <button type='button' onClick={post}>add</button>
         </form>
     }else{
         dom = <button onClick={signInWithGoogle}>login</button>
     }
      return dom
+    }
+    const post = () =>{
+       Api.addTodo(inputName,currentUser.currentUser.uid)
     }
     return(
         <div>
