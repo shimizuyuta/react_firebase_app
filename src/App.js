c8d6201e8c4a557352080040b8e9dcdd593572d8
clean_water <shmizuyuta213@icloud.com>
2021-10-10 17:34:21 +0900

    footer作成

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import './service/firebase';
 import Header from './components/Header';
 import {  AuthProvider } from './providers/AuthProvider'
 import DashBoard from './components/Dashboard';
+import Footer from './components/footer';
 
 function App() {
   return (
    <AuthProvider>
-         <div className="App">
+      <div className="App">
       <Header/>
       <DashBoard/>
-
+      <Footer/>
     </div>
    </AuthProvider>
   );
